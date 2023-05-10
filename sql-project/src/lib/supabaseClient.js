80031036d4657a676803deefc5d86fa95bd2e248
pengshi2 <boogiesnoogawoot@gmail.com>
2023-05-10 10:13:20 -0400

    vvv

---
+++ sql-project/src/lib/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://iyeixawrxvzmoxxbpzxe.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5ZWl4YXdyeHZ6bW94eGJwenhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMwMzY5MjcsImV4cCI6MTk5ODYxMjkyN30.9FM4Ttf_ephzJkxIhH5Q9kpG0oYSdkw7AvTW-On89lk></eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5ZWl4YXdyeHZ6bW94eGJwenhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMwMzY5MjcsImV4cCI6MTk5ODYxMjkyN30.9FM4Ttf_ephzJkxIhH5Q9kpG0oYSdkw7AvTW-On89lk'
+)
