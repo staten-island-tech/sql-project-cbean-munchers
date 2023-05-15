bd7bc2862a3158af0f9c5dcea6c913b3fe295b24
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-05-15 10:15:48 -0400

    vv

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -12,12 +12,18 @@ async function getData() {
 onMounted(() => {
   getData()
 })
+
+function filteredList() {
+  return users.filter((username) => username.toLowerCase().includes(input.value.toLowerCase()))
+}
 </script>
 
 <template>
-  <ul>
-    <li v-for="user in users" :key="user.id">
-      {{ user.username }}
-    </li>
-  </ul>
+  <input type="text" v-model="input" placeholder="Search users..." />
+  <div class="user" v-for="user in filteredList()" :key="user.id">
+    <p>{{ user }}</p>
+  </div>
+  <div class="item error" v-if="input && !filteredList().length">
+    <p>No results found!</p>
+  </div>
 </template>
