d989ad1de012c3f6db2a7f281c91c429d37d77ac
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-05-16 10:17:06 -0400

    asdf

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -12,13 +12,26 @@ async function getData() {
 onMounted(() => {
   getData()
 })
-
 function filteredList() {
-  return users.filter((username) => username.toLowerCase().includes(input.value.toLowerCase()))
+  return data.filter((users) => username.toLowerCase().includes(input.value.toLowerCase()))
 }
 </script>
 
 <template>
+  <!-- <ul>
+    <input type="text" v-model="input" placeholder="Search users..." />
+    <div class="user" v-for="user in users" :key="user.id">
+      <p>{{ user }}</p>
+    </div>
+    <li v-for="user in users" :key="user.id">
+      {{ user.username }}
+      #{{ user.tagline }} Current Rank :{{ user.current_rank }} Peak Rank :{{
+        user.peak_rank
+      }}
+      Playtime in hours: {{ user.playtime }}
+    </li>
+  </ul> -->
+
   <input type="text" v-model="input" placeholder="Search users..." />
   <div class="user" v-for="user in filteredList()" :key="user.id">
     <p>{{ user }}</p>
