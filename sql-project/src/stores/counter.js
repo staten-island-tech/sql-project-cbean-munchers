19d7712f5d22c2f123615b1a08ed05d92cad87f6
pengshi2 <boogiesnoogawoot@gmail.com>
2023-05-23 10:09:27 -0400

    c

--- sql-project/src/stores/counter.js
+++ sql-project/src/stores/counter.js
@@ -1,41 +1,8 @@
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { supabase } from '../lib/supabase'
 
-export const useAuthStore = defineStore('user', () =>{
+export const useAuthStore = defineStore('user', () => {
   const user = ref(null)
-  const error = ref(null)
-
-  const fetchUser = async () => {
-     user.value = await supabase.auth.user();
-
-  }
-  const signUp = async (email, password, username) => {
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-    },
-    {
-      data: { username: username}
-    }
-    )
-    if (error) {
-      error.value = error.message;
-    }
-  }
-  const signIn = async (email, password) => {
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-    if (error) {
-      error.value = error.message;
-    } else {
-      user.value = data;
-    }
-  }
-  const signOut = async () => {
-    await supabase.auth.signOut();
-    user.value = null;
-  }
-}
+  const name = ref('Jonas')
+})
