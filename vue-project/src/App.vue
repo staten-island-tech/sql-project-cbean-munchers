fcfdb2e593b1355deaef7e27ea2c37809db7f3d0
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-05-25 10:14:36 -0400

    dfgh

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,77 +1,15 @@
-<!-- <script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const basicdata = ref([])
-
-async function getData() {
-  const { data } = await supabase.from('userbasicdata').select()
-  basicdata.value = data
-  console.log(basicdata)
-}
-
-onMounted(() => {
-  getData()
-})
-
-// function filteredList() {
-//   return data.filter((userbasicdata) =>
-//     userbasicdata.username.toLowerCase().includes(input.value.toLowerCase())
-//   )
-// }
-</script>
-
 <template>
-  <ul>
-    <input type="text" v-model="search" placeholder="Search users..." />
-    <div v-for="user in basicdata" :key="user.id">
-      <Routerlink to="/"> {{ user.username }} </Routerlink>
-    </div>
-  </ul>
-</template> -->
-
-<template>
-  <!-- Check if user is available in the store, if not show auth compoenent -->
-  <Auth v-if="!store.state.user" />
-  <!-- If user is available, show the Hello World component -->
+  <div></div>
 </template>
 
 <script>
-import Auth from './components/Auth.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import { store } from './components/store'
 import { supabase } from './lib/supabaseClient'
 
 export default {
-  components: {
-    HelloWorld,
-    Auth
-  },
   setup() {
-    store.state.user = supabase.auth.user()
-
-    supabase.auth.onAuthStateChange((event, session) => {
-      if (event == 'SIGNED_OUT') {
-        store.state.user = null
-      } else {
-        store.state.user = session.user
-      }
-    })
-
-    return {
-      store
-    }
+    return {}
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss" scoped></style>
--- vue-project/src/components/SignUp.vue
+++ vue-project/src/components/SignUp.vue
@@ -1,49 +1,13 @@
 <template>
-  <div>
-    <h2>Sign up for an account</h2>
-    <form @submit.prevent="handleSignup">
-      <div>
-        <label for="email">Email</label>
-        <input id="email" type="email" v-model="email" />
-      </div>
-      <div>
-        <label for="password">Password</label>
-        <input id="password" type="password" v-model="password" />
-      </div>
-      <div>
-        <button type="submit">Sign up</button>
-      </div>
-    </form>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { supabase } from '../lib/supabaseClient'
-
 export default {
   setup() {
-    const email = ref('')
-    const password = ref('')
-
-    const handleSignup = async () => {
-      try {
-        // Use the Supabase provided method to handle the signup
-        const { error } = await supabase.auth.signUp({
-          email: email.value,
-          password: password.value
-        })
-        if (error) throw error
-      } catch (error) {
-        alert(error.error_description || error.message)
-      }
-    }
-
-    return {
-      email,
-      password,
-      handleSignup
-    }
+    return {}
   }
 }
 </script>
+
+<style lang="scss" scoped></style>
