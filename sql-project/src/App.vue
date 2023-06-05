7d703ab2b5a5e1c54805aba9a46cadbc2e6bbc53
Tommy <ytommy0111@aol.com>
2023-06-05 09:37:09 -0400

    align

---
+++ sql-project/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabase.js'
+import inputInfo from './components/inputInfo.vue'
+const players = ref([])
+const ranks = ref([])
+
+async function getPlayers() {
+  let { data } = await supabase.from('userbasicdata').select('username')
+  players.value = data
+}
+
+async function getRank() {
+  let { data } = await supabase.from('userbasicdata').select('current_rank')
+  ranks.value = data
+}
+onMounted(() => {
+  getPlayers()
+  getRank()
+})
+</script>
+
+<template>
+  <ul>
+    <input field="text" placeholder="Name" />
+    <button type="submit" value="submit"></button>
+    <li v-for="player in players" :key="player.id">
+      {{ player.username }}
+      {{ player.peak_rank }}
+    </li>
+  </ul>
+  <div>
+    <ul>
+      <li v-for="rank in ranks" :key="rank.id">
+        {{ rank.current_rank }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
