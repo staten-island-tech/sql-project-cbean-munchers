f4be6b9c820b37ec05b6863236b54a0003fdd7bf
Tommy <ytommy0111@aol.com>
2023-06-05 09:59:10 -0400

    alignagain

---
+++ sql-project/vue-project/src/stores/counter.js
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia'
+import supabase from '../lib/supabaseClient'
+export const useUserStore = defineStore('auth', {
+  state: () => {
+    return {
+      currentuser: null
+    }
+  },
+
+  actions: {
+    loaduser() {
+      this.currentuser = supabase.auth.user()
+    }
+  },
+  clearuser() {
+    this.currentuser = null
+  },
+  getters: {
+    isAuthenticated() {
+      return !!this.currentuser
+    }
+  }
+})
