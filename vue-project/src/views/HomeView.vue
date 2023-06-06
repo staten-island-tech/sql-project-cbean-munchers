e046ea06a7ba48966dcc56a3d1f370e6bdc9379e
Tommy <ytommy0111@aol.com>
2023-06-06 10:14:08 -0400

    uyg

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -2,33 +2,42 @@
 import { ref, onMounted } from 'vue'
 import { supabase } from '../lib/supabaseClient'
 
-const basicstats = ref([])
+const patientname = ref([])
 
 async function getData() {
-  const { data } = await supabase.from('userbasicdata').select()
-  basicstats.value = data
-  console.log(basicstats)
+  const { data } = await supabase.from('patientname').select()
+  patientname.value = data
+  console.log(patientname)
 }
 
-const indepth = ref([])
+const timegoing = ref([])
 
 async function GetData() {
-  const { data } = await supabase.from('userindepthstats').select()
-  indepth.value = data
-  console.log(indepth)
+  const { data } = await supabase.from('timegoing').select()
+  timegoing.value = data
+  console.log(timegoing)
 }
 
-const agentstats = ref([])
+const dategoing = ref([])
 
 async function GEtData() {
-  const { data } = await supabase.from('useragentstats').select()
-  agentstats.value = data
-  console.log(agentstats)
+  const { data } = await supabase.from('dategoing').select()
+  dategoing.value = data
+  console.log(dategoing)
+}
+
+const reasongoing = ref([])
+
+async function GETData() {
+  const { data } = await supabase.from('reasongoing').select()
+  reasongoing.value = data
+  console.log(reasongoing)
 }
 
 onMounted(() => {
   getData()
   GetData()
   GEtData()
+  GETData()
 })
 </script>
