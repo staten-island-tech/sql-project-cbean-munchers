d84239309ac2ab5720b2a04aecb99d4c4f1993c1
Tommy <ytommy0111@aol.com>
2023-06-07 10:16:00 -0400

    route

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,22 +1,41 @@
+import Home from '../pages/Home.vue'
+import LogIn from '../pages/LogIn.vue'
+import SignUp from '../pages/SignUp.vue'
+
 import { createRouter, createWebHistory } from 'vue-router'
+import { userStore } from '../stores/user'
+
+const routes = [
+  { path: '/', component: LogIn },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LogIn
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/SignUpPage',
-      name: 'SignUpPage',
-      component: () => import('../views/SignUpPage.vue')
-    },
-    {
-      path: '/SignInPage',
-      name: 'SignInPage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SignInPage.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes: routes
 })
 
+router.beforeEach((to, from, next) => {
+  const user = userStore()
+  console.log(to)
+  if (to.name === 'home' && user.user === null) {
+    next('/login')
+  } else {
+    next()
+  }
+})
 export default router
